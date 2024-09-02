1539e112ff2c6d671cd11565c6568fe39decef00
nehabn <20becs087.nehabn@gmail.com>
2024-09-02 22:50:03 +0530

    Add styling, add pause for testing & tag validation

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware()
       .concat(albumsApi.middleware);
-  }
+  },
 });
 
 setupListeners(store.dispatch);
