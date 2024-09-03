a02a38c2bb60e845f65b21bd2ddcb6a4fab67cce
nehabn <20becs087.nehabn@gmail.com>
2024-09-03 22:21:46 +0530

    Add remove album feature

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,26 @@
+import { GoTrashcan } from 'react-icons/go';
+import { useRemoveAlbumMutation } from '../store';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  }
+  const header = 
+  <>
+    <Button className onClick={handleRemoveAlbum} >
+      <GoTrashcan />
+    </Button>
+    {album.title}
+  </>;
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of photos in the album
+    </ExpandablePanel>
+  );
+}
+export default AlbumsListItem;
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -19,6 +19,17 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: 'Album', id: album.userId }];
+        },
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: 'DELETE',
+          }
+        }
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: 'Album', id: user.id }];
@@ -52,5 +63,9 @@ const albumsApi = createApi({
   }
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const { 
+  useFetchAlbumsQuery, 
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export {albumsApi};
